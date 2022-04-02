7b5e3e792761987bef81fc54e329fec8e51b35f1
TRUNG <dotientrung201030@gmail.com>
2022-04-02 21:05:59 +0700

    Add FE login and register

--- route/index.route.js
+++ route/index.route.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('home');
-})
+});
+
+router.get('/login',(req,res) => {
+    res.render('login');
+});
+
+router.get('/register', (req,res) => {
+    res.render('register');
+});
 
 export default router;
